5ecb94e0b58c4abbfc20b5f743b32110c46ae356
Derzan Chiang <derzanchiang1800@gmail.com>
2022-10-02 15:32:13 -0500

    update

--- index.js
+++ index.js
@@ -33,17 +33,16 @@ async function processSVG(req, res) {
     const shadow = req.query.shadow || "1";
     const label = req.query.label || "VISITS";
     const uniqueID = req.params.uniqueID;
-    const swap = req.query.swap;
+    const swap = eq.query.swap || "0";
 
     // Get the current visits count
     const visits = await database.visitsBadge(uniqueID);
 
     // Create the SVG Badge
-    let svg = svgBadge(label, shadow, labelBGColor, countBGColor, labelTextColor, countTextColor, visits);
-    if (swap === "1") {
-        svg = svgBadge(visits, shadow, labelBGColor, countBGColor, labelTextColor, countTextColor, label);
-    }
-    
+    let svg = swap === "0"
+        ? svgBadge(label, shadow, labelBGColor, countBGColor, labelTextColor, countTextColor, visits)
+        : svgBadge(visits, shadow, labelBGColor, countBGColor, labelTextColor, countTextColor, label);
+
     // Send the SVG Badge
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(svg);
